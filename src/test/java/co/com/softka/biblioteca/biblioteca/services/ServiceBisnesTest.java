05bdb4cf351480a021cbcd55e9f2e25906d8904b
Juan carlos salcedo <juank9225@gmail.com>
2021-06-30 12:28:13 -0500

    Se realizan dos test para el serviceBisnes , recomendar tipo y disponibilidad

--- src/test/java/co/com/softka/biblioteca/biblioteca/services/ServiceBisnesTest.java
+++ src/test/java/co/com/softka/biblioteca/biblioteca/services/ServiceBisnesTest.java
@@ -1,10 +1,8 @@
 package co.com.softka.biblioteca.biblioteca.services;
 
 import co.com.softka.biblioteca.biblioteca.collections.Recurso;
-import co.com.softka.biblioteca.biblioteca.dtos.RecursoDTO;
 import co.com.softka.biblioteca.biblioteca.repositories.RepositoryRecurso;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,20 +14,39 @@ import static org.mockito.ArgumentMatchers.any;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ServiceRecursoCRUDTest {
+class ServiceBisnesTest {
 
     @MockBean
     private RepositoryRecurso repositoryRecurso;
 
     @Autowired
-    private ServiceRecursoCRUD serviceRecursoCRUD;
+    private ServiceBisnes serviceBisnes;
 
     @Test
-    @DisplayName("Test findAll Success")
-    public void obtenerTodos(){
+    public void getDisponibilidad(){
+
+        var recurso1 = new Recurso();
+        recurso1.setId("61db30202deddb25ca8010f9");
+        recurso1.setNombre("amor en tiempos oscuros");
+        recurso1.setTipoRecurso("libro");
+        recurso1.setFecha(LocalDate.now());
+        recurso1.setDisponible(true);
+        recurso1.setAreaTematicaId("51db30202deddb25ca8010f5");
+
+        Mockito.when(repositoryRecurso.findById(any())).thenReturn(java.util.Optional.of(recurso1));
+        var resultado = serviceBisnes.disponibilidad("51db30202deddb25ca8010f5");
+
+        Assertions.assertEquals(true, resultado.getDisponible());
+
+    }
+
+    @Test
+    public void getRecomendarTipo(){
+
         var recurso1 = new Recurso();
         recurso1.setId("61db30202deddb25ca8010f9");
         recurso1.setNombre("amor en tiempos oscuros");
@@ -49,42 +66,13 @@ class ServiceRecursoCRUDTest {
         var lista = new ArrayList<Recurso>();
         lista.add(recurso1);
         lista.add(recurso2);
-        Mockito.when(repositoryRecurso.findAll()).thenReturn(lista);
+        Mockito.when(repositoryRecurso.findBytipoRecurso("libro")).thenReturn(java.util.Optional.of(lista));
 
-        var resultado = serviceRecursoCRUD.obtenerTodos();
+        var resultado = serviceBisnes.recomendarPorTipo("libro");
 
         Assertions.assertEquals(2, resultado.size());
-        Assertions.assertEquals(recurso1.getNombre(), resultado.get(0).getNombre());
+        Assertions.assertEquals(recurso1.getTipoRecurso(), resultado.get(0).getTipoRecurso());
         Assertions.assertEquals(recurso2.getNombre(), resultado.get(1).getNombre());
-    }
-
-    @Test
-    @DisplayName("Test save Success")
-    public void crear(){
-        var recurso1 = new Recurso();
-        recurso1.setId("61db30202deddb25ca8010f9");
-        recurso1.setNombre("amor en tiempos oscuros");
-        recurso1.setTipoRecurso("libro");
-        recurso1.setFecha(LocalDate.now());
-        recurso1.setDisponible(true);
-        recurso1.setAreaTematicaId("51db30202deddb25ca8010f5");
-
-        var recurso2 = new RecursoDTO();
-        recurso2.setId("61db30202deddb25ca8010f9");
-        recurso2.setNombre("amor en tiempos oscuros");
-        recurso2.setTipoRecurso("libro");
-        recurso2.setFecha(LocalDate.now());
-        recurso2.setDisponible(true);
-        recurso2.setAreaTematicaId("51db30202deddb25ca8010f5");
-
-        Mockito.when(repositoryRecurso.save(any())).thenReturn(recurso1);
-
-        var resultado = serviceRecursoCRUD.crear(recurso2);
-
-        Assertions.assertEquals(recurso1.getNombre(), resultado.getNombre());
-        Assertions.assertEquals(recurso1.getTipoRecurso(), resultado.getTipoRecurso());
-        Assertions.assertEquals(recurso1.getAreaTematicaId(),"51db30202deddb25ca8010f5");
 
     }
-
 }
